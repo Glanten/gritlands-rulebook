f0068f06c8cb0f17646cd991f58e0580d87de974
Glanten <tern.glanten@gmail.com>
2024-04-15 19:44:40 +0100

    Created stylesheet and JavaScript files; implemented pop-out side navigation bar

---
+++ static/layout.js
@@ -0,0 +1,11 @@
+/* open the sidenav menu to 250px */
+function openNav() {
+    document.getElementById("sidenavmenu").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+  
+/* close the sidenav menu to 0 */
+function closeNav() {
+    document.getElementById("sidenavmenu").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
