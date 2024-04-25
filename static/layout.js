80550c55d453a84c10baf9184b8d09cdcc07ab81
Glanten <tern.glanten@gmail.com>
2024-04-25 07:32:34 +0100

    changed navbar from programmatically-generated (was causing issues) to statically-typed. This will allow other elements in the navbar (section headers, dividers, etc.)

--- static/layout.js
+++ static/layout.js
@@ -21,86 +21,59 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-/* introducing page content */
+/* INTRODUCING PAGE CONTENT */
 
 // get list of links from side navbar
 document.addEventListener('DOMContentLoaded', function() {
 
-  const links = [
-    { id: 'homepage', text: 'Introduction', url: 'static/pagecontent/home.html' },
-    { id: 'characters', text: 'Characters', url: 'static/pagecontent/characters.html' },
-    { id: 'clans', text: 'Clans', url: 'static/pagecontent/clans.html' },
-    { id: 'archetypes', text: 'Archetypes', url: 'static/pagecontent/archetypes.html' },
-    { id: 'grit', text: 'GRIT', url: 'static/pagecontent/grit.html' },
-    { id: 'health', text: 'Health', url: 'static/pagecontent/health.html' },
-    { id: 'equipment', text: 'Equipment', url: 'static/pagecontent/equipment.html' },
-    { id: 'armour', text: 'Armour', url: 'static/pagecontent/armour.html' },
-    { id: 'tasks', text: 'Tasks', url: 'static/pagecontent/tasks.html' },
-    { id: 'tags', text: 'Tags', url: 'static/pagecontent/tags.html' },
-    { id: 'hotzones', text: 'HOTZONES', url: 'static/pagecontent/hotzones.html' },
-    { id: 'chargen', text: 'Character Creation', url: 'static/pagecontent/chargen.html' },
-    { id: 'consequences', text: 'Consequences', url: 'static/pagecontent/consequences.html' },
-    { id: 'disclaimers', text: 'Disclaimers', url: 'static/pagecontent/disclaimers.html' },
-  ];
+  /* HISTORY & PSEUDO-COOKIES */
+  // check if there is a page in user's history
+  const lastVisitedPage = localStorage.getItem('lastVisitedPage');
+  if (lastVisitedPage) {
+    loadContent(lastVisitedPage);
+    // try this also for the page's subtitle
+    document.getElementById('pagesubtitle').textContent = localStorage.getItem('lastVisitedSubtitle');
+  } else {
+    // if there is no stored page, load the introduction
+    loadContent('static/pagecontent/home.html');
+    document.getElementById('pagesubtitle').textContent = 'Introduction';
+  }
 
-  // Populate the sidebar with links
-  const sidebar = document.getElementById('sidenavmenu');
-  const pageSubtitle = this.getElementById('pagesubtitle')
-  links.forEach(function(linkData) {
-    const link = document.createElement('a');
-    link.href = '#';
-    link.classList.add('contentlink');
-    link.setAttribute('data-url', linkData.url);
-    link.id = linkData.id;
-    link.textContent = linkData.text;
-
-    // Add click event listener to close the sidebar and load content
-    (function(link) {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        closeNav();
-        loadContent(linkData.url);
-        // store URL of clicked link in localStorage
-        localStorage.setItem('lastVisitedPage', linkData.url);
-        localStorage.setItem('lastVisitedSubtitle', linkData.text);
-        pageSubtitle.textContent = linkData.text;
-      });
-    })(link);
-    // Append the link to the sidebar
-    sidebar.appendChild(link);
+  /* LINKS IN THE POP-OUT NAVBAR */
+  // get all elements with the class "contentlink"
+  const links = document.querySelectorAll('.contentlink');
+  // add click event listener for each link
+  links.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      // prevent default link behaviour
+      event.preventDefault();
+      // get URL from data-url attribute
+      const url = this.getAttribute('data-url');
+      const pageSubtitle = this.getAttribute('data-title')
+      // load content from URL
+      loadContent(url);
+      localStorage.setItem('lastVisitedPage', url);
+      localStorage.setItem('lastVisitedSubtitle', pageSubtitle);
+      document.getElementById('pagesubtitle').textContent = pageSubtitle;
+      closeNav();
+    });
   });
 
-  // Function to fetch and display content from an HTML file
-  function loadContent(url, sub) {
+  /* FETCH AND DISPLAY CONTENT FROM "STATIC" HTML PAGES */
+  function loadContent(url) {
     fetch(url)
       .then(response => {
         if (!response.ok) {
-          throw new Error('ERROR: Could not reach page!');
+          throw new Error('Not network response');
         }
         return response.text();
-    })
-    .then(text => {
-      // Display the fetched content in the 'maincontent' div
-      document.getElementById('maincontent').innerHTML = text;
-    })
-    .catch(error => {
-      loadContent("static/pagecontent/404.html", "404");
-      console.error('ERROR: Problem with fetch operation:', error);
-    });
-  }
-
-  // load page last visited (if there is one)
-  const lastVisitedPage = localStorage.getItem("lastVisitedPage");
-  if (lastVisitedPage) {
-    const lastVisitedSubtitle = localStorage.getItem("lastVisitedSubtitle");
-    pageSubtitle.textContent = lastVisitedSubtitle;
-    loadContent(lastVisitedPage);
-  } else {
-    const defaultPage = "static/pagecontent/home.html";
-    const defaultSub = "Home";
-    pageSubtitle.textContent = defaultSub;
-    loadContent(defaultPage, "Home");
-    sessionStorage.setItem('lastVisitedPage', defaultPage);
-    sessionStorage.setItem('lastVisitedSubtitle', defaultSub);
+      })
+      .then(text => {
+        // display fetched content in the main page
+        document.getElementById("maincontent").innerHTML = text;
+      })
+      .catch(error => {
+        console.error('ERROR:', error);
+      })
   }
 });
