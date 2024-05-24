a9bd64e6f0bda94f5760bfd2e62fa4af726cf40a
Glanten <tern.glanten@gmail.com>
2024-05-24 17:37:59 +0100

    rectifying filename formats.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // register the service worker to enable PWA behaviour
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(
+  navigator.serviceWorker.register("sw.js").then(
     (registration) => {
       console.log("Service worker registration successful:", registration);
     },
